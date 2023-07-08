09ffc2ad50e1fe214e11f89f97e48b3e778f4328
ForteFortissmo <94246167+ForteFortissmo@users.noreply.github.com>
2023-07-07 21:52:34 -0400

    Factory health bar/can heal/be destroyed, factory damage sfx added, player movement tuned
    what the title says

--- GMTK2023/Assets/Scripts/FactoryBehavior.cs
+++ GMTK2023/Assets/Scripts/FactoryBehavior.cs
@@ -1,44 +1,90 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FactoryBehavior : MonoBehaviour
 {
     [SerializeField] private float health = 100f;
+    [SerializeField] private float maxhealth = 100f;
     [SerializeField] private float decrease = 1f; //How fast the health decreases when you stand next to it
+    [SerializeField] private float increase = 0.5f; //How fast the health increases when you're not standing next to it
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private bool isTouchingPlayer;
+    public GameObject hpSlider; //slider object on canvas
+    public Sprite Untouched;
     public Sprite halfDestroyed;
     public Sprite Destroyed;
+    public AudioClip[] DamageSounds;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //update HP bar
+        hpSlider.GetComponent<Slider>().value = health / maxhealth;
+
+        //heals when not touching player
+        if (!isTouchingPlayer && health < maxhealth)
+        {
+            health += increase * Time.deltaTime;
+        }
 
         //Destroy self when health 0 and set sprite to different states of destruction
-        if (health <= 50f && health > 0)
+        if (health <= (maxhealth * 0.5f) && health > (maxhealth * 0.25f))//half health
         {
-
             gameObject.GetComponent<SpriteRenderer>().sprite = halfDestroyed;
-
         }
-        else if(health <= 0f)
+        else if (health <= (maxhealth * 0.25f) && health > (0)) //quarter health
         {
             gameObject.GetComponent<SpriteRenderer>().sprite = Destroyed;
-            //Destroy(gameObject);
+        }
+        else if (health <= 0f)//death
+        {
+            Destroy(hpSlider);
+            Destroy(gameObject);
+        }
+        else //healed
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = Untouched;
         }
 
     }
-    
+
     void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag == "Player" && health > 0.0f)
+        {
+            health -= decrease * Time.deltaTime;
+
+            //play a damage sound at random if one isn't already playing
+            if (!audioSource.isPlaying)
+            {
+                int _randomNum = Random.Range(0, DamageSounds.Length);
+                audioSource.pitch = (Random.Range(0.8f, 1.2f));
+                audioSource.PlayOneShot(DamageSounds[_randomNum]);
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
-            health -= decrease * Time.deltaTime;
+            isTouchingPlayer = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            isTouchingPlayer = false;
         }
     }
 }
