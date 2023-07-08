0bbb39f7d2d9db56382ae161f922473f8db68239
bussyboixoxo <m.neri343@gmail.com>
2023-07-07 20:16:57 -0400

    Added factory art and switching between factory states

--- GMTK2023/Assets/Scripts/FactoryBehavior.cs
+++ GMTK2023/Assets/Scripts/FactoryBehavior.cs
@@ -6,6 +6,8 @@ public class FactoryBehavior : MonoBehaviour
 {
     [SerializeField] private float health = 100f;
     [SerializeField] private float decrease = 1f; //How fast the health decreases when you stand next to it
+    public Sprite halfDestroyed;
+    public Sprite Destroyed;
 
     // Start is called before the first frame update
     void Start()
@@ -16,11 +18,20 @@ public class FactoryBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Destroy self when health 0
-        if (health <= 0f)
+
+        //Destroy self when health 0 and set sprite to different states of destruction
+        if (health <= 50f && health > 0)
         {
-            Destroy(gameObject);
+
+            gameObject.GetComponent<SpriteRenderer>().sprite = halfDestroyed;
+
         }
+        else if(health <= 0f)
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = Destroyed;
+            //Destroy(gameObject);
+        }
+
     }
     
     void OnTriggerStay2D(Collider2D other)
