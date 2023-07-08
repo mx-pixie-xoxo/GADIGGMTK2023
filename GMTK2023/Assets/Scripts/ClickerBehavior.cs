b0ab695561880c01af87143fbe17167ae0f224cf
Hannah Jackowski <hannah.r.jackowski@gmail.com>
2023-07-08 17:56:09 -0400

    Attacks now fall from the top of the screen.

--- GMTK2023/Assets/Scripts/ClickerBehavior.cs
+++ GMTK2023/Assets/Scripts/ClickerBehavior.cs
@@ -11,17 +11,15 @@ public class ClickerBehavior : MonoBehaviour
     private GameObject _goldenCookie;
 
     // Attack stuff
-    public float PlayerFoundTimeout = 0.5f;
     public float AttackTimeout = 10.0f;
     public float FactorySpawnRange = 5.0f;
     public float MoveTimeout = 1.0f;
     public GameObject FactoryPrefab;
+    public GameObject AttackPrefab;
 
     [SerializeField]
     private float _attackTimeoutDelta;
-    private float _playerFoundDelta;
     private float _moveTimeoutDelta;
-    private bool _inRange;
 
     public enum ClickerState : int {
         Idle,           // 0
@@ -48,29 +46,18 @@ public class ClickerBehavior : MonoBehaviour
         {
             case ClickerState.FollowPlayer:
                 MoveTo(_player);
+                if (_attackTimeoutDelta <= 0) ClickAttack();
                 break;
             case ClickerState.GoldenCookie:
                 MoveTo(_goldenCookie);
                 break;
-            case ClickerState.Attack:
+            case ClickerState.Attack:   // change to idle?
                 _moveTimeoutDelta -= Time.deltaTime;
                 if (_moveTimeoutDelta <= 0) _state = ClickerState.FollowPlayer;
                 break;
         }
 
         if (_attackTimeoutDelta >= 0) _attackTimeoutDelta -= Time.deltaTime;
-
-        if (_inRange && _state == ClickerState.FollowPlayer)
-        {
-            _playerFoundDelta -= Time.deltaTime;
-
-            if (_playerFoundDelta <= 0 && _attackTimeoutDelta <= 0) 
-            {
-                _state = ClickerState.Attack;
-                _attackTimeoutDelta = AttackTimeout;
-                SpawnFactory();
-            }
-        }
     }
 
     private void MoveTo(GameObject target)
@@ -80,29 +67,26 @@ public class ClickerBehavior : MonoBehaviour
         transform.position = Vector2.MoveTowards(transform.position, target.transform.position, step);
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.tag == "Player")
-        {
-            _inRange = true;
-            _playerFoundDelta = PlayerFoundTimeout;
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.tag == "Player")
-        {
-            _inRange = false;
-        }
-    }
-
-    private void SpawnFactory()
+    public void SpawnFactory()
     {
         Vector2 randomLoc = Random.insideUnitCircle * FactorySpawnRange;
         Vector3 spawnLoc = new Vector3(randomLoc.x, randomLoc.y, 0.0f) + transform.position;
         
         GameObject newFac = Instantiate(FactoryPrefab, spawnLoc, Quaternion.identity) as GameObject;
+    }
+
+    private void ClickAttack()
+    {   
+        // spawn click attack
+        GameObject spawning = Instantiate(AttackPrefab, transform.position, Quaternion.identity) as GameObject;
+        ClickerAttack attack = spawning.GetComponent<ClickerAttack>();
+        attack.Target = transform.position;
+        attack.Clicker = this;
+        
+
+        _attackTimeoutDelta = AttackTimeout;
+        _moveTimeoutDelta = MoveTimeout;
+        _state = ClickerState.Attack;
     }
 
 }
